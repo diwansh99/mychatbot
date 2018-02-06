674df5cfb6fd352dd520c83000fd83910e7ec6e0
Diwansh Gulyani <diwansh@Diwanshs-MacBook-Air.local>
2018-02-06 16:13:30 +0530

    my first commit

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ var port = 5000 || process.env.PORT;
 app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
 var VERIFY_TOKEN = '1234';
+var ACCESS_TOKEN ='EAAYdsMqggCIBAFxlHR52UmYMQfb3fuUMUK0GZB5Na8HZBrksRbVPyW5t3mQJouARtvfOcUavsKBMpB5lsI5peXgUEbsUhvL7S8u8lNGOOPD02uTgb1vxnvPQ9szgstPrqplvqkH8KtpxuFYQ0stlEmA01pf0N8ToiwmhqRmGBRwaZC5R0NV';
+var APP_SECRET = 'some secret string of numbers and letters';
+
 
 
 app.get('/' , function(req,res){
@@ -40,3 +43,52 @@ app.get('/webhook/', function(req, res) {
 app.listen(port , function(){
   console.log("Server is running on" + port);
 });
+
+
+const bot = new Bootbot({
+  accessToken = ACCESS_TOKEN;
+  verifyToken = VERIFY_TOKEN;
+  appSecret = APP_SECRET;
+})
+
+bot.setGreetingText("Hello , I am here to solve your problems")
+
+bot.setGetStartedButton((payload , chat) => {
+  if(config.bucket === undefined)
+  {
+    chat.say("Hello my name is Note Buddy and I am here to help you")
+
+  }
+  BotUserId = payload.sender.id
+}
+);
+
+bot.hear('setup' , (payload,chat) => {
+  const getBucketSlug = (convo) => {
+    convo.ask("what's your Bucket slug?" , (payload , convo) =>{
+      var slug = payload.message.text;
+      convo.set('slug' , slug);
+      convo.say("Setting Slug as" + slug).then(() =>
+      getBucketReadKey(convo));
+    })
+  }
+
+  getBucketReadKey = (convo) =>{
+    convo.ask("What's your Bucket Readkey?" , (payload , convo) => {
+      var Readkey = payload.message.text;
+      convo.set('read_key' , Readkey);
+      convo.say("Setting Read_key as" + Readkey).then(() =>
+    getBucketWriteKey(convo));
+    })
+  }
+
+  getBucketWriteKey = (convo) =>{
+    convo.ask("What's your Bucket Writekey?" , (payload,convo) => {
+      var Writekey = payload.message.text;
+      convo.set('write_key' , Writekey);
+      convo.say("Setting Write_key as" + Writekey).then(()=>
+    finishing(convo));
+    })
+  }
+  
+})
